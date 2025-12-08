7f7a844c2e35a26d1c864ea9704853ccfec928d9
José Olávio da Silva Ronald Perin Rauta <jose.os2006@aluno.ifsc.edu.br>
2025-12-07 22:01:34 -0300

    Lendo o DAO

--- app/src/main/java/com/example/aula2/PlanetaDAO.java
+++ app/src/main/java/com/example/aula2/PlanetaDAO.java
@@ -1,13 +1,20 @@
+//em resumo, aqui os dados ficam guardados num banco simulado e envia o que as classes solicitam
+
 package com.example.aula2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PlanetaDAO {
+    //os dados ficam nesse array
     private final ArrayList<Planeta> planetas;
 
     public PlanetaDAO() {
+        //começa com a lista vazia
         planetas = new ArrayList<>();
+
+        //coloca os dados na lista
+        //o R.drawable ali só diz o nome (ID) da imagem que vai adicionar
         planetas.add(new Planeta("Sol", R.drawable.sun));
         planetas.add(new Planeta("Mercúrio", R.drawable.mercury));
         planetas.add(new Planeta("Vênus", R.drawable.venus));
@@ -19,6 +26,7 @@ public class PlanetaDAO {
         planetas.add(new Planeta("Netuno", R.drawable.neptune));
     }
 
+    //retorna planetas pra geral, por isso é publico
     public ArrayList<Planeta> getPlanetas() {
         return planetas;
     }
