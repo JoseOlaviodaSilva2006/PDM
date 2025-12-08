5820512fb331133b1b87a1260a3dd99190c7143e
José Olávio da Silva Ronald Perin Rauta <jose.os2006@aluno.ifsc.edu.br>
2025-12-07 21:57:31 -0300

    Lendo o controller e entendendo como o array ta funcionando
    Refactor getNomePlanetas method for clarity.

--- app/src/main/java/com/example/aula2/PlanetaController.java
+++ app/src/main/java/com/example/aula2/PlanetaController.java
@@ -1,23 +1,29 @@
+//essa classe separa o view do DAO
+
 package com.example.aula2;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class PlanetaController {
+    //ele inicializa o DAO
     private final PlanetaDAO planetaDAO;
 
     public PlanetaController() {
         this.planetaDAO = new PlanetaDAO();
     }
 
+    //
     public ArrayList<Planeta> getPlaneta() {
+        //como o controller não tem dados, ele retorna o dao
         return planetaDAO.getPlanetas();
     }
 
     public ArrayList<String> getNomePlanetas() {
-        return planetaDAO.getPlanetas()
-                .stream()
-                .map(Planeta::getNome)
-                .collect(Collectors.toCollection(ArrayList::new));
+        //o controller pega os dados brutos e vai transformando
+        return planetaDAO.getPlanetas() //pega a lista
+                .stream() //cria um fluxo
+                .map(Planeta::getNome) //pega só o nome de cada objeto
+                .collect(Collectors.toCollection(ArrayList::new)); //transforma numa nova lista
     }
 }
